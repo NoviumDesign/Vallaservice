9a92c81b919949950b61eba8e746e29ed7ff281c
Emil Blomquist <mr.emil.blomquist@gmail.com>
2013-02-04 18:49:49 +0100

    comapnyadmin.js fix
    Will work with the new server configuration

--- companyadmin.js
+++ companyadmin.js
@@ -1,7 +1,7 @@
 // variables
-var url, company, business, token, formId, success;
+var url, company, business, token, formId, success, customer;
 // functions
-var getFormData, getToken, buildForm;
+var getFormData, getToken, buildForm, validate;
 
 // constants
 url = 'http://companyadmins.elasticbeanstalk.com';
@@ -10,19 +10,23 @@ business = '43g5345hj3';
 formId = '#order';
 success = '<div class="success"><h2>Tack för din beställning!</h2></div>';
 
-// first
+
+// vaiables data
+customer = false,
+
+
+// token request
 getToken = function() {
 	$.getJSON(
 		url + '/request/token',
 		function(response) {
 			token = response.token;
 			
-
 			getFormData();
 	});
 }
 
-// second
+// products request
 getFormData = function() {
 	$.getJSON(
 		url + '/request/data/' + company + '/' + business + '/' + token,
@@ -33,69 +37,68 @@ getFormData = function() {
 	});
 }
 
+// build product select
 buildForm = function(data) {
 	for(var i = 0; i < data.products.length; i++) {
 
-		$(formId + ' .products').append(
-				'<option data-secret="' + data.products[i].product_secret + '" value="1">' +
-	        		data.products[i].product + ' ' + data.products[i].price + 'kr' + 
-	            '</option>'
-			);
+		if(i == 0) {
+			// set product to first product
+			$(formId + ' .products').attr('name', 'products[' + data.products[i].product_secret + ']');
+		}
+
+		$(formId + ' select.products').append(
+			'<option data-secret="' + data.products[i].product_secret + '" value="1">' +
+        		data.products[i].product + (data.products[i].price > 0? ' ' + data.products[i].price + 'kr': '') +
+            '</option>'
+		);
 	}
 }
 
+// vaildate form
+validate = function() {
+	var valid = true;
+
+	// product
+	$productsFields = $(formId + ' section.products input, ' + formId + ' section.products select');
+	$productsFields.each(function() {
+		if($(this).val() == undefined || $(this).val() == '') {
+			valid = false;
+		}
+	});
+
+	// new customer
+	if(!customer) {
+		$customerFields = $(formId + ' section.newCustomer input, ' + formId + ' section.newCustomer select');
+		$customerFields.each(function() {
+			if($(this).val() == undefined || $(this).val() == '') {
+				valid = false;
+			}
+		});
+	}
+
+	// return
+	if(valid) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// product secret
 $(formId + ' .products').change(function() {
 	var secret = $(this).find(":selected").data('secret');
 
 	$(this).attr('name', 'products[' + secret + ']');
 });
 
-// toggle section
-$(formId + ' input[name="toggleCustomer"]').change(function() {
-	var value, newCustomer, oldCustomer;
-
-	value = $(this).val();
-	newCustomer = $(formId + ' .newCustomer');
-	oldCustomer = $(formId + ' .oldCustomer');
-
-	if(value == 'true') {
-		newCustomer.removeClass('hidden');
-		oldCustomer.addClass('hidden');
-	} else {
-		oldCustomer.removeClass('hidden');
-		newCustomer.addClass('hidden');
-	}
-});
-
-
-// try customer
-var running = false;
-$(formId + ' .try').keyup(function() {
-	var value = $(formId + ' .try').val();
-
-	if(value.length > 4 && running == false) {
-		running = true;
-		$.post(
-			url + '/request/customer/' + company + '/' + business + '/' + token,
-        	{'try' : value},
-        	function(response){
-        		running = false;
-
-        		token = response.token;
-				
-				if(response.customer) {
-					$(formId + ' .selectedCustomer').html(response.customer.name);
-				} else {
-					$(formId + ' .selectedCustomer').html('Ingen matchning');
-				}
-
-	    }, 'json');
-	}
-});
-
-
+// submit
 $(formId).submit(function(event) {
 	event.preventDefault();
+
+	if(!validate()) {
+		alert('Var vänlig och fyll i alla fält med röd stjärna');
+		return;
+	}
 
     $.post(url + '/request/post/' + company + '/' + business + '/' + token,
     	$(this).serialize(),
@@ -105,11 +108,11 @@ $(formId).submit(function(event) {
 			if(response.success) {
 				$(formId).html(success);
 			} else if(response.error) {
-				if(response.error = 'secret') {
-					alert('Kundkoden du valt funkar tyvärr inte.');
-				} else {
-					alert('Formuläret är inte rätt ifyllt!');
-				}
+				// if(response.error = 'secret') {
+				// 	alert('Kundkoden du valt funkar tyvärr inte.');
+				// } else {
+					alert('Formuläret är inte rätt ifyllt');
+				// }
 			}                  
     }, 'json');
 });
@@ -126,6 +129,51 @@ $(formId + ' .quantity').change(function() {
 	$(formId + ' select.products').children().attr('value', value)
 });
 
+// start
 $(document).ready(function() {
 	getToken();
 });
+
+// toggle section
+// $(formId + ' input[name="toggleCustomer"]').change(function() {
+// 	var value, newCustomer, oldCustomer;
+
+// 	value = $(this).val();
+// 	newCustomer = $(formId + ' .newCustomer');
+// 	oldCustomer = $(formId + ' .oldCustomer');
+
+// 	if(value == 'true') {
+// 		newCustomer.removeClass('hidden');
+// 		oldCustomer.addClass('hidden');
+// 	} else {
+// 		oldCustomer.removeClass('hidden');
+// 		newCustomer.addClass('hidden');
+// 	}
+// });
+
+// try customer
+// var running = false;
+// $(formId + ' .try').keyup(function() {
+// 	var value = $(formId + ' .try').val();
+
+// 	if(value.length > 4 && running == false) {
+// 		running = true;
+// 		$.post(
+// 			url + '/request/customer/' + company + '/' + business + '/' + token,
+//         	{'try' : value},
+//         	function(response){
+//         		running = false;
+
+//         		token = response.token;
+				
+// 				if(response.customer) {
+// 					$(formId + ' .selectedCustomer').html(response.customer.name);
+// 					customer = true;
+// 				} else {
+// 					$(formId + ' .selectedCustomer').html('Ingen matchning');
+// 					customer = false;
+// 				}
+
+// 	    }, 'json');
+// 	}
+// });
